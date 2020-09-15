a072391d2b881d576223ae90695bfdc0a0f6f835
Daniel Krysztofiak <danielk@cloudwhale.nl>
2020-09-15 16:02:06 +0200

    Release only branch

--- Horrogue/Assets/Scripts/Rendering/KeepUpright.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class KeepUpright : MonoBehaviour {
-	void Update () {
-        transform.rotation = Quaternion.Euler(0,0,0);
-	}
-}
